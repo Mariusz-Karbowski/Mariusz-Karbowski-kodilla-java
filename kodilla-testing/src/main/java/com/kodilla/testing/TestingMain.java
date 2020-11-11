5b5b62dfe3485c66067f3d3c0caa56c98729e32e
Mariusz-Karbowski <m.karbowski@outlook.com>
2020-11-11 16:15:21 +0100

    6.2 Added first unit tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("SimpleUSer test OK");
+        } else {
+            System.out.println("SimpleUser test failed!");
+        }
+
+        System.out.println("\nTest - first unit test:");
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 5;
+        int sum = calculator.add(a, b);
+        if( sum == 10) {
+            System.out.println("Task 'a + b' correctly executed.");
+        } else {
+            System.out.println("Task execution failed!");
+        }
+
+        System.out.println("\nTest - second unit test:");
+        int substraction = calculator.substract(a, b);
+        if( substraction == 0) {
+            System.out.println("Task 'a - b' correctly executed.");
+        } else {
+            System.out.println("Task execution failed!");
+        }
     }
 }
