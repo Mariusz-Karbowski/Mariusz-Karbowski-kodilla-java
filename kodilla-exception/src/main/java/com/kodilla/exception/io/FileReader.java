f8714756ffc67ce360ecefa08fd3100395a2e8c0
Mariusz-Karbowski <m.karbowski@outlook.com>
2020-11-26 12:12:16 +0100

    8.2 try! catch! finally!

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,6 +1,7 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,9 +13,13 @@ public class FileReader {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
-        Path path = Paths.get(file.getPath());
-        Stream<String> fileLines = Files.lines(path);
 
-        System.out.println(file.getPath());
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Błąd odczytu pliku" + e);
+        } finally {
+            System.out.println("I'm gonna be here... always!");
+        }
     }
 }
