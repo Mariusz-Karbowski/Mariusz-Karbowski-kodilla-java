41819b62ef9a56f0a317e16299eff819fbfc49f6
Mariusz-Karbowski <m.karbowski@outlook.com>
2021-01-12 20:26:45 +0100

    13.1 Functional iteration over the titles

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApp.java
@@ -0,0 +1,36 @@
+package com.kodilla.good.patterns.challenges;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MovieStoreApp {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        Map<String, List<String>> movieMap = movieStore.getMovies();
+
+        movieMap.entrySet()
+                .stream()
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue().stream().collect(Collectors.joining("!"))));
+
+        final String moviesTitles = movieMap.entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("!"));
+        System.out.println(moviesTitles);
+
+        final String moviesTitlesPlus = movieMap.entrySet()
+                .stream()
+                .flatMap(n -> {
+                    List<String> movieList = new ArrayList<>();
+                    movieList.add(n.getKey());
+                    movieList.addAll(n.getValue());
+                    return movieList.stream();
+                })
+                .collect((Collectors.joining("!")));
+        System.out.println(moviesTitlesPlus);
+    }
+}
